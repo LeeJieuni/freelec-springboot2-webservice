f26cafac1b8cee888b0c8936afd8e0d770e20ae8
jieun <lgeun123@naver.com>
2022-06-27 23:52:41 +0900

    Alter chapter2

---
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Posts { //실제 DB 테이블과 매칭될 클래스
+
+
+
+}
